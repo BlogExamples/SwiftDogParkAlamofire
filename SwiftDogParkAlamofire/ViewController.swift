a0017de33460d7fac9313794fdd5dabe11829575
Josh Kuehn <joshmkuehn@gmail.com>
2017-05-22 15:06:12 -0500

    Initial Commit

---
+++ SwiftDogParkAlamofire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDogParkAlamofire
+//
+//  Created by JoshuaKuehn on 5/22/17.
+//  Copyright © 2017 edu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
